61ab43bb220b97a4cc3cd780a009e3533267373b
Enes Yıldırım <enes5519@gmail.com>
2021-09-04 13:32:35 +0300

    feat: use image component of nextjs

--- src/components/core/Header/Navigation/index.js
+++ src/components/core/Header/Navigation/index.js
@@ -4,7 +4,11 @@ import NavigationItem from '@/components/core/Header/Navigation/NavigationItem';
 
 const Navigation = () => (
   <nav className="hidden lg:flex items-center gap-8 h-full text-xl min-w-0">
-    <Link href="/" class="hover:text-red-500">Anasayfa</Link>
+    <Link href="/">
+      <a className="hover:text-red-500">
+        Anasayfa
+      </a>
+    </Link>
     {navigationItems.map((navigationItem) => <NavigationItem key={navigationItem.id} navigationItem={navigationItem} />)}
   </nav>
 );
--- src/components/core/Header/index.js
+++ src/components/core/Header/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import Navigation from '@/components/core/Header/Navigation';
 
 const Header = () => (
@@ -7,25 +8,25 @@ const Header = () => (
       <div className="h-12 container items-center flex justify-center lg:justify-between">
         <div className="flex items-center space-x-4">
           <a href="https://www.facebook.com/AksPro55/" target="_blank" rel="noreferrer">
-            <img src="/icons/facebook.svg" alt="Facebook Logo" />
+            <Image src="/icons/facebook.svg" alt="Facebook Logo" width={24} height={24} />
           </a>
           <a href="https://twitter.com/AksPro55" target="_blank" rel="noreferrer">
-            <img src="/icons/twitter.svg" alt="Twitter Logo" />
+            <Image src="/icons/twitter.svg" alt="Twitter Logo" width={24} height={24} />
           </a>
           <a href="https://api.whatsapp.com/send?phone=+905333736155" target="_blank" rel="noreferrer">
-            <img src="/icons/whatsapp.svg" alt="WhatsApp Logo" />
+            <Image src="/icons/whatsapp.svg" alt="WhatsApp Logo" width={24} height={24} />
           </a>
           <a href="https://www.instagram.com/akspro55/" target="_blank" rel="noreferrer">
-            <img src="/icons/instagram.svg" alt="Instagram Logo" />
+            <Image src="/icons/instagram.svg" alt="Instagram Logo" width={24} height={24} />
           </a>
         </div>
         <div className="flex items-center space-x-4 lg:space-x-8 ml-4">
           <a className="flex items-center space-x-2" href="mailto:info@akspro.com.tr">
-            <img src="/icons/mail.svg" alt="Mail Icon" />
+            <Image src="/icons/mail.svg" alt="Mail Icon" width={24} height={24} />
             <span className="hidden lg:block">info@akspro.com.tr</span>
           </a>
           <a className="flex items-center space-x-2" href="tel:+903622385858">
-            <img src="/icons/mail.svg" alt="Phone Icon" />
+            <Image src="/icons/mail.svg" alt="Phone Icon" width={24} height={24} />
             <span className="hidden lg:block">+90 362 238 5858</span>
           </a>
         </div>
@@ -33,10 +34,12 @@ const Header = () => (
     </section>
     <section className="container h-24 flex items-center justify-between border-b-1 border-grey-500">
       <Link href="/">
-        <img src="/logo-text.png" alt="Aks &amp; Pro Logo" className="h-14 lg:h-16" />
+        <a className="relative w-[190.44px] h-14 lg:w-[217.65px] lg:h-16">
+          <Image src="/logo-text.png" alt="Aks &amp; Pro Logo" layout="fill" />
+        </a>
       </Link>
       <div className="lg:hidden">
-        <img src="/icons/hamburger-menu.svg" alt="Menu Icon" />
+        <Image src="/icons/hamburger-menu.svg" alt="Menu Icon" width={24} height={24} />
       </div>
       <Navigation />
     </section>
